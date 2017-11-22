3255977358b360e15e52f0f7556e863afc9eb1f2
周旭智 <zhouxuzhi@xunlei.com>
2017-11-22 11:42:20 +0800

    Updated by xz: change source url

--- apt/apt_source_update_1404.sh
+++ apt/apt_source_update_1404.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mv /etc/apt/sources.list /etc/apt/sources.list.bak && wget -P /etc/apt https://raw.githubusercontent.com/df007df/docker_workspace/e065d67082df7fda04aa8c0019cf26c8d5689008/redis/sources.list && apt-get update
+mv /etc/apt/sources.list /etc/apt/sources.list.bak && wget -P /etc/apt https://raw.githubusercontent.com/EdwardChou/utility/master/apt/14.04/sources.list && apt-get update
